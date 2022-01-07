83d004ae41c516e66838b6d7129ce93a8797a33c
douglaswfbrandao00 <douglaswfbrandao@gmail.com>
2022-01-07 17:25:53 -0300

    test: nsure SignUpController throws if no name is provided

---
+++ test/add-user.spec.ts
@@ -0,0 +1,19 @@
+import { SignUpController } from '../src/presentation/SignUpController'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const userCandidate = {
+      name: 'any_name',
+      email: 'any_email@nanana.com',
+      password: 'any_password',
+      confirmPassword: 'any_password'
+    }
+
+    const sut = new SignUpController()
+
+    const httpResponse = sut.handle(userCandidate)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param name'))
+  })
+});
