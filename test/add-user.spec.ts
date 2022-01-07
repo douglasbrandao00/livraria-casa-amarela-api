50bec83bfa889c7a12ff07f29f79678463278f0a
douglaswfbrandao00 <douglaswfbrandao@gmail.com>
2022-01-07 17:45:35 -0300

    test: ensure SignUpController throws if no email is provided

--- test/add-user.spec.ts
+++ test/add-user.spec.ts
@@ -3,7 +3,6 @@ import { SignUpController } from '../src/presentation/SignUpController'
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const userCandidate = {
-      name: 'any_name',
       email: 'any_email@nanana.com',
       password: 'any_password',
       confirmPassword: 'any_password'
@@ -11,9 +10,25 @@ describe('SignUp Controller', () => {
 
     const sut = new SignUpController()
 
-    const httpResponse = sut.handle(userCandidate)
+    const httpResponse = sut.handle({body: userCandidate})
 
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('missing param name'))
+    expect(httpResponse.body).toEqual(new Error('missing param: name'))
   })
+  test('Should return 400 if no email is provided', () => {
+    const userCandidate = {
+      name: 'any_name',
+      //email: 'any_email@nanana.com',
+      password: 'any_password',
+      confirmPassword: 'any_password'
+    }
+
+    const sut = new SignUpController()
+
+    const httpResponse = sut.handle({body: userCandidate})
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param: email'))
+  })
+
 });
