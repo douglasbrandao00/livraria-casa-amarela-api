f33a2a4a11b0e7c7672a9bd581af8191207512a3
douglaswfbrandao00 <douglaswfbrandao@gmail.com>
2022-01-10 01:40:48 -0300

    test: ensure book/RegisterController return 400 if no title is provided

---
+++ test/presentation/books/register-controller.spec.ts
@@ -0,0 +1,37 @@
+import { RegisterBookController } from 'App/presentation/books/register-controller'
+import {MissingParamError} from 'App/presentation/erros'
+import {badRequest} from 'App/presentation/helpers/http-helper'
+import {HttpRequest} from 'App/presentation/protocols'
+
+function makeBookCandidate(): HttpRequest {
+  return {
+    body: {
+      title: "clean code",
+      subtitle: "A Handbook of Agile Software Craftsmanship",
+      author: 'Robert C. Martin',
+      description: 'Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way.'
+    }  
+  }
+}
+
+function makeSut() {
+  const sut = new RegisterBookController()
+  
+  return {
+    sut
+  }
+}
+
+describe('books/RegisterController', () => {
+  test('Should return 400 if no title is provided', async () => {
+    const bookCandidate = makeBookCandidate()
+    bookCandidate.body.title = ''
+
+    const { sut } = makeSut()
+
+    const res = await sut.handle(bookCandidate)
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toEqual(new MissingParamError('title'))
+  });
+})
