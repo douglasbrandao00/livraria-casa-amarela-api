5ea2e4ebb32dd5bb0b7c4b53238b4e1afa788f65
douglaswfbrandao00 <douglaswfbrandao@gmail.com>
2022-01-10 01:40:21 -0300

    feat: ensure book/RegisterController return 400 if no title is provided

---
+++ src/presentation/books/register-controller.ts
@@ -0,0 +1,24 @@
+import { Controller, HttpRequest, HttpResponse } from "../protocols";
+import { badRequest, created } from '../helpers/http-helper'
+import {MissingParamError} from "../erros";
+
+export class RegisterBookController implements Controller {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const missingParam = this.hasMissingParam(httpRequest)
+
+    if(missingParam) {
+      return badRequest(new MissingParamError(missingParam))
+    }
+    return created({})
+  }
+
+  hasMissingParam(httpRequest: HttpRequest): string | void {
+    const requiredParams = ['title']
+
+    for( const param of requiredParams ) {
+      if(!httpRequest.body[param]) {
+        return param
+      }
+    }
+  }
+}
