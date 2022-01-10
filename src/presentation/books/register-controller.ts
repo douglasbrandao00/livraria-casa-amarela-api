dcfe7b0024676541bcd0ef5e6bd124bbc5f0c79d
douglaswfbrandao00 <douglaswfbrandao@gmail.com>
2022-01-10 03:08:24 -0300

    feat: ensure book/RegisterController return 500 checkAccountByEmailRepository.check throws

--- src/presentation/books/register-controller.ts
+++ src/presentation/books/register-controller.ts
@@ -1,6 +1,6 @@
 import { Controller, HttpRequest, HttpResponse } from "../protocols";
-import { badRequest, notAcceptable, created } from '../helpers/http-helper'
-import {BookAlreadyRegistredError, MissingParamError} from "../erros";
+import { badRequest, notAcceptable, created, internalServerError } from '../helpers/http-helper'
+import {BookAlreadyRegistredError, MissingParamError, ServerError} from "../erros";
 import {CheckIsBookRegistredRepository} from "App/domain/repository/book/check-is-book-registred";
 
 export type RegisterBookControllerInput = {
@@ -9,19 +9,22 @@ export type RegisterBookControllerInput = {
 export class RegisterBookController implements Controller {
   constructor(private readonly input: RegisterBookControllerInput){}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const missingParam = this.hasMissingParam(httpRequest)
+    try {
+      const missingParam = this.hasMissingParam(httpRequest)
 
-    if(missingParam) {
-      return badRequest(new MissingParamError(missingParam))
+      if(missingParam) {
+        return badRequest(new MissingParamError(missingParam))
+      }
+      const { title } = httpRequest.body
+      const isRegistred = await this.input.checkAccountByEmailRepository.check(title)
+      if(isRegistred) {
+        return notAcceptable(new BookAlreadyRegistredError(title))
+      }
+      return created({})
+    } catch(_error: any) {
+      return internalServerError(new ServerError)
     }
-    const { title } = httpRequest.body
-    const isRegistred = await this.input.checkAccountByEmailRepository.check(title)
-    if(isRegistred) {
-      return notAcceptable(new BookAlreadyRegistredError(title))
-    }
-    return created({})
   }
-
   hasMissingParam(httpRequest: HttpRequest): string | void {
     const requiredParams = ['title', 'author', 'description']
 
