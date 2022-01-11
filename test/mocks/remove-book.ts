feac389c400de97ecda66852e00d1504b25da1c3
douglaswfbrandao00 <douglaswfbrandao@gmail.com>
2022-01-11 13:14:19 -0300

    test: ensure DeleteControler works properly

--- test/mocks/remove-book.ts
+++ test/mocks/remove-book.ts
@@ -3,8 +3,7 @@ import {RemoveBook} from 'App/domain/use-cases/book/remove-book'
 export class RemoveBookMock implements RemoveBook {
   input?: string
   output?: Error
-  // @ts-ignore
-  remove(bookId: string): Promise<void | Error> {
+  async remove(bookId: string): Promise<void | Error> {
     this.input = bookId
     if(this.output) {
       return new Promise(_ => this.output as Error)
